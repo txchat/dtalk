ff68504f9807f213e649d7fcd4a5436d3b633f0d
dld <oofpgDLD@gmail.com>
2022-04-02 11:45:10 +0800

    fix: api authmidware use auth pkg

--- pkg/api/midware.go
+++ pkg/api/midware.go
@@ -1,19 +1,12 @@
 package api
 
 import (
-	"crypto/sha256"
-	"encoding/base64"
-	"fmt"
-	"net/http"
-	"reflect"
-	"strconv"
-	"strings"
-
 	"github.com/gin-gonic/gin"
 	zlog "github.com/rs/zerolog/log"
-	"github.com/txchat/dtalk/pkg/address"
+	"github.com/txchat/dtalk/pkg/auth"
 	xerror "github.com/txchat/dtalk/pkg/error"
-	"github.com/txchat/dtalk/pkg/util"
+	"net/http"
+	"reflect"
 )
 
 var log = zlog.Logger
@@ -104,23 +97,23 @@ func AuthMiddleWare() gin.HandlerFunc {
 			context.Set(DeviceName, deviceName)
 			context.Set(Version, version)
 		} else {
-			pubKey, err := VerifyAddress(sig)
+			server := auth.NewDefaultApuAuthenticator()
+			uid, err := server.Auth(sig)
 			if err != nil {
+				switch err {
+				case auth.ERR_SIGNATUREINVALID:
+					err = xerror.NewError(xerror.SignatureInvalid)
+				case auth.ERR_SIGNATUREEXPIRED:
+					err = xerror.NewError(xerror.SignatureExpired)
+				}
 				log.Debug().Err(err).Msg("VerifyAddress failed")
-				context.Set(ReqError, err)
-				context.Abort()
-				return
-			}
-			addr := address.PublicKeyToAddress(address.NormalVer, pubKey)
-			if addr == "" {
-				log.Debug().Msg("PublicKeyToAddress addr is empty")
 				context.Set(ReqError, err)
 				context.Abort()
 				return
 			}
 			//set val
 			context.Set(Signature, sig)
-			context.Set(Address, addr)
+			context.Set(Address, uid)
 			context.Set(Uuid, uuid)
 			context.Set(DeviceType, device)
 			context.Set(DeviceName, deviceName)
@@ -142,51 +135,4 @@ func HeaderMiddleWare() gin.HandlerFunc {
 		context.Set(DeviceName, deviceName)
 		context.Set(Version, version)
 	}
-}
-
-//get pubKey
-func VerifyAddress(str string) ([]byte, error) {
-	//<signature>#<msg>#<address>; <>
-	ss := strings.SplitN(str, "#", -1)
-	if len(ss) < 3 {
-		log.Debug().Err(fmt.Errorf("need length:%v,got:%v", 3, len(ss))).Str("sig", str).Msg("split signature failed")
-		return nil, xerror.NewError(xerror.SignatureInvalid)
-	}
-	sigData := ss[0]
-	msgData := ss[1]
-	pubKeyData := ss[2]
-
-	msg := strings.SplitN(msgData, "*", -1)
-	if len(msg) < 2 {
-		log.Debug().Err(fmt.Errorf("need msg length:%v,got:%v", 2, len(ss))).Str("msgData", msgData).Msg("split msg data failed")
-		return nil, xerror.NewError(xerror.SignatureInvalid)
-	}
-	time, err := strconv.ParseInt(msg[0], 10, 64)
-	if err != nil {
-		log.Debug().Err(err).Str("datetime", msg[0]).Msg("ParseInt datetime failed")
-		return nil, xerror.NewError(xerror.SignatureInvalid)
-	}
-	//secp256
-	sig, err := base64.StdEncoding.DecodeString(sigData)
-	if err != nil {
-		log.Debug().Err(err).Str("sigData", sigData).Msg("base64 decode sig data failed")
-		return nil, xerror.NewError(xerror.SignatureInvalid)
-	}
-	pubKey, err := util.HexDecode(pubKeyData)
-	if err != nil {
-		log.Debug().Err(err).Str("pubKeyData", pubKeyData).Msg("hex decode pubKey failed")
-		return nil, xerror.NewError(xerror.SignatureInvalid)
-	}
-	msg256 := sha256.Sum256([]byte(msgData))
-	if !util.Secp256k1Verify(msg256[:], sig, pubKey) {
-		log.Debug().Err(err).Str("msgData", msgData).Bytes("sig", sig).Bytes("pubKey", pubKey).
-			Msg("Secp256k1Verify failed")
-		return nil, xerror.NewError(xerror.SignatureInvalid)
-	}
-	//检查时间是否过期
-	if util.CheckTimeOut(time, HeaderTimeOut) {
-		log.Debug().Err(err).Int64("time", time).Msg("verify timeout")
-		return nil, xerror.NewError(xerror.SignatureExpired)
-	}
-	return pubKey, nil
 }
--- pkg/auth/apiauthenticator_test.go
+++ pkg/auth/apiauthenticator_test.go
@@ -20,8 +20,51 @@ func TestDefaultApuAuthenticator(t *testing.T) {
 	client := NewDefaultApuAuthenticator()
 	sig := client.Request("dtalk", pubKey, privKey)
 	server := NewDefaultApuAuthenticator()
-	if !server.Auth(sig) {
-		t.Fail()
+	uid, err := server.Auth(sig)
+	if err != nil {
+		t.Error(err)
 	}
+	t.Logf("uid:%s", uid)
+	t.Log("success")
+}
+
+func TestDefaultApuAuthenticatorRequest(t *testing.T) {
+	pubKey, err := hex.DecodeString("02b2dcf40123a5364a4bc9fd717db92122f90321a6771a47bc922100c9852c8b68")
+	if err != nil {
+		t.Errorf("decode pubKey:%v", err)
+		return
+	}
+	privKey, err := hex.DecodeString("bfae31775aeefb2eb01f604e2a4cf6d6c4cb4c072ddfbde03235252bd2765e06")
+	if err != nil {
+		t.Errorf("decode privKey:%v", err)
+		return
+	}
+
+	client := NewDefaultApuAuthenticator()
+	sig := client.Request("dtalk", pubKey, privKey)
+	t.Logf("sig:%s", sig)
+}
+
+func TestDefaultApuAuthenticatorAuthInvalid(t *testing.T) {
+	sig := "UXmp9mdD/iQlkAXqAHHzWaFwTNOxv6OxyqqSoJo8RnD6kJmh1dCF3f8GKL70/LCZKucvwAgUWbj9rTVXeq3gA=#1648784849078376*dtalk#02b2dcf40123a5364a4bc9fd717db92122f90321a6771a47bc922100c9852c8b68"
+	server := NewDefaultApuAuthenticator()
+	uid, err := server.Auth(sig)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("uid:%s", uid)
+	t.Log("success")
+}
+
+func TestDefaultApuAuthenticatorAuthExpire(t *testing.T) {
+	sig := "G0UXmp9mdD/iQlkAXqAHHzWaFwTNOxv6OxyqqSoJo8RnD6kJmh1dCF3f8GKL70/LCZKucvwAgUWbj9rTVXeq3gA=#1648784849078376*dtalk#02b2dcf40123a5364a4bc9fd717db92122f90321a6771a47bc922100c9852c8b68"
+	server := NewDefaultApuAuthenticator()
+	uid, err := server.Auth(sig)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("uid:%s", uid)
 	t.Log("success")
 }
