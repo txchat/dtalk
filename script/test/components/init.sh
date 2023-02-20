bdd510b6d4b1e2a21da7962f4463b0ee32add123
oofpgDLD <oofpgdld@gmail.com>
2023-02-20 19:45:04 +0800

    add unitest shell

---
+++ script/test/components/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+work_dir=$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)/../../../
+
+cp -R ./ "${work_dir}/test_compose/"
+cp -R "${work_dir}/script/mysql/." "${work_dir}/test_compose/"
+cp -R "${work_dir}/script/nginx/." "${work_dir}/test_compose/"
+
+chmod +x "${work_dir}/test_compose/startup.sh"
+chmod +x "${work_dir}/test_compose/shutdown.sh"
---
+++ script/test/unitestenv/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+work_dir=$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)/../../../
+
+cp -R "${work_dir}/script/test/unitestenv/" "${work_dir}/test_uni/"
+cp -R "${work_dir}/script/mysql/." "${work_dir}/test_uni/"
+cp -R "${work_dir}/script/nginx/." "${work_dir}/test_uni/"
+
+chmod +x "${work_dir}/test_uni/wait-for-it.sh"
+
+if [ ! -d "${work_dir}/test_uni/" ]; then
+    echo "runtime file not exists"
+    exit 1
+fi
+
+envOsArch=$(go version | awk '{ print $4 }')
+targetOS=$(echo "${envOsArch}" | awk -F '/' '{ print $1 }')
+targetARCH=$(echo "${envOsArch}" | awk -F '/' '{ print $2 }')
+
+exportGOEnv() {
+    export GOOS=${targetOS}
+    export GOARCH=${targetARCH}
+    export CGO_ENABLED=0
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.cn,direct
+    export GOSUMDB='sum.golang.google.cn'
+}
+
+exportGOEnv
+
+#docker compose -f components.compose.yaml up && $(GOENV) go test -v "${work_dir}"/...
+#docker compose -f components.compose.yaml down
+
+cd "${work_dir}/test_uni/" && docker compose -f components.compose.yaml up -d && ./wait-for-it.sh
+
+#go test -v "${work_dir}"/...
+
+# shutdown
+docker compose -f components.compose.yaml down
