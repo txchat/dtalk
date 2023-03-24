88747ad44a6e3f095fceb4300cc9bf327db7fa38
oofpgDLD <oofpgdld@gmail.com>
2023-03-24 18:21:22 +0800

    version rpc dao use gorm and add test

--- app/services/version/internal/dao/versionrepomysql.go
+++ app/services/version/internal/dao/versionrepomysql.go
@@ -3,10 +3,11 @@ package dao
 import (
 	"context"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/txchat/dtalk/app/services/version/internal/model"
-	xerror "github.com/txchat/dtalk/pkg/error"
-	"github.com/txchat/dtalk/pkg/mysql"
 	"github.com/txchat/dtalk/pkg/util"
+	gorm_mysql "gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 const (
@@ -17,150 +18,124 @@ const (
 	_QueryOtherReleaseVersionInfo    = `SELECT * FROM dtalk_ver_backend WHERE id!=? AND device_type=? AND platform=? AND state=1`
 	_ChangeOtherReleaseVersionStatus = `UPDATE dtalk_ver_backend SET state=0,update_time=?,ope_user=? WHERE id=?`
 	_GetVersionList                  = `SELECT * FROM dtalk_ver_backend WHERE platform LIKE ? AND device_type LIKE ? ORDER BY create_time DESC LIMIT ?,?`
-	_GetVersionCount                 = `SELECT COUNT(*) FROM dtalk_ver_backend WHERE platform LIKE ? AND device_type LIKE ?`
+	_GetVersionCount                 = `SELECT COUNT(*) AS version_count FROM dtalk_ver_backend WHERE platform LIKE ? AND device_type LIKE ?`
 	_GetForceVersionCount            = `SELECT COUNT(*) AS force_num FROM dtalk_ver_backend WHERE device_type=? AND platform=? AND force_update = 1 AND version_code > ? AND version_code < ?`
 	_GetReleaseVersionInfo           = `SELECT * FROM dtalk_ver_backend WHERE device_type=? AND platform=? AND state = 1 ORDER BY id DESC LIMIT 1`
 	_QueryVersionInfoById            = `SELECT * FROM dtalk_ver_backend WHERE id=?`
 )
 
 type VersionRepositoryMysql struct {
-	conn *mysql.MysqlConn
+	db *gorm.DB
 }
 
-func NewVersionRepositoryMysql(conn *mysql.MysqlConn) *VersionRepositoryMysql {
+func NewVersionRepositoryMysql(mysqlConfig mysql.Config) *VersionRepositoryMysql {
+	if mysqlConfig.Params == nil {
+		mysqlConfig.Params = make(map[string]string)
+	}
+	mysqlConfig.Params["charset"] = "UTF8MB4"
+	dsn := mysqlConfig.FormatDSN()
+	db, err := gorm.Open(gorm_mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 	return &VersionRepositoryMysql{
-		conn: conn,
+		db: db,
 	}
 }
 
 func (repo *VersionRepositoryMysql) GetVersionInfo(ctx context.Context, vid int64) (*model.VersionForm, error) {
-	records, err := repo.conn.Query(_GetVersionInfo, vid)
-	if err != nil {
-		return nil, err
-	}
-	if len(records) < 1 {
-		return nil, xerror.ErrNotFound
-	}
-	record := records[0]
-	version, err := model.ConvertVersionForm(record)
-	if err != nil {
-		return nil, err
-	}
-	return version, nil
+	var version model.VersionForm
+	err := repo.db.Raw(_GetVersionInfo, vid).Take(&version).Error
+	return &version, err
 }
 
 func (repo *VersionRepositoryMysql) AddVersionInfo(ctx context.Context, form *model.VersionForm) (int64, int64, error) {
-	num, lastId, err := repo.conn.Exec(_InsertVersionInfo, form.Platform, form.Status, form.DeviceType, form.VersionCode, form.VersionName, form.URL, form.Force, form.Size, form.Md5, form.Description.ToString(), form.OpeUser, form.UpdateTime, form.CreateTime)
-	return num, lastId, err
+	err := repo.db.Exec(_InsertVersionInfo, form.Platform, form.Status, form.DeviceType, form.VersionCode, form.VersionName, form.URL, form.Force, form.Size, form.Md5, form.Description.ToString(), form.OpeUser, form.UpdateTime, form.CreateTime).Error
+	return repo.db.RowsAffected, 0, err
 }
 
 func (repo *VersionRepositoryMysql) UpdateVersionInfo(ctx context.Context, form *model.VersionForm) (int64, int64, error) {
-	num, lastId, err := repo.conn.Exec(_UpdateVersionInfo, form.VersionName, form.VersionCode, form.URL, form.Size, form.Md5, form.Description.ToString(), form.Force, form.UpdateTime, form.OpeUser, form.Id)
-	return num, lastId, err
+	err := repo.db.Exec(_UpdateVersionInfo, form.VersionName, form.VersionCode, form.URL, form.Size, form.Md5, form.Description.ToString(), form.Force, form.UpdateTime, form.OpeUser, form.Id).Error
+	return repo.db.RowsAffected, 0, err
 }
 
 // ReleaseSpecificVersion 将指定版本id的记录状态改为上线，同时将与之同平台和设备类型的版本号的状态置为下线。
 func (repo *VersionRepositoryMysql) ReleaseSpecificVersion(ctx context.Context, vid, updateTime int64, operator string) error {
-	tx, err := repo.conn.NewTx()
-	if err != nil {
-		return err
-	}
-	_, _, err = tx.Exec(_ChangeVersionStatus, updateTime, operator, vid)
-	if err != nil {
+	tx := repo.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
 		return err
 	}
 
-	records, err := tx.Query(_QueryVersionInfoById, vid)
-	if err != nil {
+	if err := tx.Exec(_ChangeVersionStatus, updateTime, operator, vid).Error; err != nil {
+		tx.Rollback()
 		return err
 	}
-	if len(records) == 0 {
-		return xerror.ErrNotFound
-	}
-	deviceType := records[0]["device_type"]
-	platform := records[0]["platform"]
 
-	records, err = tx.Query(_QueryOtherReleaseVersionInfo, vid, deviceType, platform)
-	if err != nil {
+	var versionInfo model.VersionForm
+	if err := tx.Raw(_QueryVersionInfoById, vid).Take(&versionInfo).Error; err != nil {
+		tx.Rollback()
 		return err
 	}
-	if len(records) == 0 {
-		err = tx.Commit()
-		if err != nil {
+	var otherVersions []model.VersionForm
+	if err := tx.Raw(_QueryOtherReleaseVersionInfo, vid, versionInfo.DeviceType, versionInfo.Platform).Scan(&otherVersions).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	if len(otherVersions) < 1 {
+		return tx.Commit().Error
+	}
+	for _, version := range otherVersions {
+		id := util.MustToInt64(version.Id)
+		if err := tx.Exec(_ChangeOtherReleaseVersionStatus, updateTime, operator, id).Error; err != nil {
+			tx.Rollback()
 			return err
 		}
-		return nil
 	}
-
-	id := util.MustToInt64(records[0]["id"])
-	_, _, err = tx.Exec(_ChangeOtherReleaseVersionStatus, updateTime, operator, id)
-	if err != nil {
-		return err
-	}
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-	return nil
+	return tx.Commit().Error
 }
 
 func (repo *VersionRepositoryMysql) SpecificPlatformAndDeviceTypeVersions(ctx context.Context, platform, deviceType string, page int64, size int64) ([]*model.VersionForm, error) {
 	offset := page * size
-	records, err := repo.conn.Query(_GetVersionList, platform, deviceType, offset, size)
-	if err != nil {
+	var versions []*model.VersionForm
+	if err := repo.db.Raw(_GetVersionList, platform, deviceType, offset, size).Scan(&versions).Error; err != nil {
 		return nil, err
-	}
-
-	versions := make([]*model.VersionForm, len(records))
-	for i, record := range records {
-		version, err := model.ConvertVersionForm(record)
-		if err != nil {
-			return nil, err
-		}
-		versions[i] = version
 	}
 	return versions, nil
 }
 
 func (repo *VersionRepositoryMysql) SpecificPlatformAndDeviceTypeCount(ctx context.Context, platform, deviceType string) (int64, error) {
-	countRecords, err := repo.conn.Query(_GetVersionCount, platform, deviceType)
+	var versionCount int64
+	err := repo.db.Raw(_GetVersionCount, platform, deviceType).Scan(&versionCount).Error
 	if err != nil {
 		return 0, err
 	}
-	if len(countRecords) == 0 {
-		return 0, xerror.ErrNotFound
-	}
-	totalCount := util.MustToInt64(countRecords[0]["COUNT(*)"])
+	totalCount := util.MustToInt64(versionCount)
 	return totalCount, nil
 }
 
 func (repo *VersionRepositoryMysql) LastReleaseVersion(ctx context.Context, platform, deviceType string) (*model.VersionForm, error) {
-	records, err := repo.conn.Query(_GetReleaseVersionInfo, deviceType, platform)
+	var version model.VersionForm
+	err := repo.db.Raw(_GetReleaseVersionInfo, deviceType, platform).Scan(&version).Error
 	if err != nil {
 		return nil, err
 	}
-
-	if len(records) < 1 {
-		return &model.VersionForm{}, nil
-	}
-	record, err := model.ConvertVersionForm(records[0])
-	if err != nil {
-		return nil, err
-	}
-	return record, nil
+	return &version, nil
 }
 
 func (repo *VersionRepositoryMysql) ForceNumberBetween(ctx context.Context, platform, deviceType string, begin, end int64) (int64, error) {
 	if begin >= end {
 		return 0, nil
 	}
-	records, err := repo.conn.Query(_GetForceVersionCount, deviceType, platform, begin, end)
+	var forceNum int64
+	err := repo.db.Raw(_GetForceVersionCount, deviceType, platform, begin, end).Scan(&forceNum).Error
 	if err != nil {
 		return 0, err
 	}
-
-	if len(records) < 1 {
-		return 0, nil
-	}
-	return util.MustToInt64(records[0]["force_num"]), nil
+	return util.MustToInt64(forceNum), nil
 }
